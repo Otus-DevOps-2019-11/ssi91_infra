d98804fd6d282e1874d552f29fe45ffb14fc1c8a
ssi91 <sergeev.s.i1991@gmail.com>
2020-02-05 12:21:58 +0700

    add vm-creation script

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,10 +1,10 @@
 #!/bim/bash
 
-gcloud compute instances create reddit-app-start-up \
+gcloud compute instances create reddit-app-full \
 --boot-disk-size=10GB \
 --image-project=ubuntu-os-cloud \
 --machine-type=g1-small \
 --tags puma-server \
 --restart-on-failure \
 --image-project=infra-262720 \
---image=reddit-full-1580835443
+--image=reddit-full-1580879563
